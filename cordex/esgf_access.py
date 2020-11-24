cf7ac9b879b018fbee0caa9d496c18a754c43c54
Lars Buntemeyer <lars.buntemeyer@hzg.de>
2020-11-24 22:53:53 +0100

    updates of esgf_access

--- cordex/esgf_access.py
+++ cordex/esgf_access.py
@@ -4,6 +4,13 @@
 import pandas as pd
 
 try:
+    from tqdm import tqdm
+except:
+    def tqdm(x):
+        return x
+
+
+try:
     from pyesgf.search import SearchConnection
 except:
     print('pyesgf client is not installed! please install https://github.com/ESGF/esgf-pyclient to make use of this module.')
@@ -13,7 +20,7 @@ DKRZ_HOST = 'esgf-data.dkrz.de'
 DKRZ_ESG_URL = 'https://{}/esg-search'.format(DKRZ_HOST)
 
 #cordex  output  EUR-11  GERICS  ECMWF-ERAINT  evaluation  r1i1p1  REMO2015  v1  day  tasmax  v20180813
-CORDEX_COLUMNS = ['project_id', 'product', 'domain', 'institute', 'driving_model_id', 'experiment_id', 'member', 'model_id', 'version', 'frequency', 'variable', 'date']
+CORDEX_COLUMNS = ['project_id', 'product', 'domain', 'institute', 'driving_model_id', 'experiment_id', 'member', 'model_id', 'version', 'frequency', 'variable', 'date', 'host']
 CMIP5_COLUMNS = ['project_id', 'product', 'domain', 'institute', 'driving_model_id', 'experiment_id', 'member', 'model_id', 'version', 'frequency', 'variable', 'date']
 
 COLUMNS  = {'cordex': CORDEX_COLUMNS,
@@ -37,48 +44,59 @@ def connect(url=DKRZ_ESG_URL):
     return SearchConnection(url, distrib=True)
 
 
-
-def search(attrs, conn=None, verbose=False):
-    if conn is None:
-        return connect().new_context(**attrs).search()
-    else:
-        return conn.new_context(**attrs).search()
-
-def search_opendap(attrs, conn=None, agg=False, verbose=False):
-    """agg=True will return aggregation urls
-    instead of list of file urls...
+def context(attrs, conn=None, verbose=False):
+    """get an ESGF search context.
     """
     if conn is None:
         ctx = connect().new_context(**attrs)
     else:
-        ctx = conn.new_context(**attrs)
+        ctx =  conn.new_context(**attrs)
     if verbose: print('Hit Count: {}'.format(ctx.hit_count))
-    for facet, item in ctx.facet_counts.items():
-        if item: print('{} : {}'.format(facet, item))
+    return ctx
+
+
+def datasets(attrs, conn=None, verbose=False):
+    """returns dataset results
+    """
+    return context(attrs, conn, verbose).search()
+
+
+def dataset_dict(attrs, conn=None, verbose=False):
+    return {ds.dataset_id: ds for ds in tqdm(datasets(attrs, conn, verbose))}
+
+
+def get_opendap_urls(attrs, conn=None, agg=False, verbose=False):
+    """agg=True will return aggregation urls
+    instead of list of file urls...
+    """
+    ctx = context(attrs, conn, verbose)
+    #for facet, item in ctx.facet_counts.items():
+    #    if item: print('{} : {}'.format(facet, item))
     results = ctx.search()
     open_dap = {}
-    #print('nr of results: {}'.format(len(results)))
-    for result in results:
+    for result in tqdm(results):
         sources = []
         if verbose: print(result.dataset_id)
         if agg:
            ctx_results = result.aggregation_context().search()
         else:
            ctx_results = result.file_context().search()
-        #print('nr of aggregation results: {}'.format(len(ctx_results)))
         for ctx_result in ctx_results:
             sources.append(ctx_result.opendap_url)
-            #print(agg.opendap_url)
         open_dap[result.dataset_id] = sources
     if verbose: print('found {} datasets'.format(len(open_dap.keys())))
     return open_dap
 
 
+def _split_dataset_id(dataset_id):
+    """splits a dataset id into list of values.
+    """
+    return (dataset_id.split('|')[0].split('.') + [(dataset_id.split('|')[1])])
+
 
 def to_pandas(results):
     """convert a search result to dataframe"""
-    data = [res.dataset_id.split('|')[0].split('.') + res.dataset_id.split('|')[1] for res in results]
-    #return pd.DataFrame.from_dict(data, orient='index')
-    return pd.DataFrame(data, columns = CORDEX_COLUMNS)
+    data = [_split_dataset_id(res.dataset_id) for res in tqdm(results)]
+    return pd.DataFrame(data, columns=CORDEX_COLUMNS)
 
 
