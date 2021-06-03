f2545d846f9d95558b7ba8da8660e930fdf9ec37
github action <lars.buntemeyer@hzg.de>
2021-06-03 21:35:00 +0200

    updates for syntax highlightin in notebooks

--- docs/conf.py
+++ docs/conf.py
@@ -42,6 +42,7 @@ extensions = [
         "numpydoc",
         "nbsphinx",
         "nbsphinx_link",
+        "IPython.sphinxext.ipython_console_highlighting"
 ]
 
 autodoc_mock_imports = ["cartopy", "xesmf", "geopandas", "matplotlib", "regionmask"]
@@ -90,7 +91,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
