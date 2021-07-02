e33262401afcf9645b8c044955f4f9324226e643
github action <lars.buntemeyer@hzg.de>
2021-07-03 01:05:46 +0200

    updates

--- docs/conf.py
+++ docs/conf.py
@@ -108,6 +108,8 @@ todo_include_todos = False
 #html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
 
+html_logo = 'cordex_logo.png'
+
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
 # documentation.
