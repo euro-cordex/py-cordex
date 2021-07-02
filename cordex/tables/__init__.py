794667c03201a5df5499f9f68005819478a760a4
github action <lars.buntemeyer@hzg.de>
2021-07-02 16:27:28 +0200

    add tables module

---
+++ cordex/tables/__init__.py
@@ -0,0 +1,24 @@
+
+import pandas as pd
+
+from ._resources import read_cordex_domain_tables
+
+
+class read_cls():
+    
+    def __init__(self, reader):
+        self.reader = reader
+    
+    @property
+    def tables(self):
+        return self.reader()
+    
+    @property
+    def table(self):
+        return pd.concat(self.tables.values())
+    
+    def __getattr__(self, table):
+        return self.tables[table]
+
+
+domains = read_cls(read_cordex_domain_tables)
