f03fb85476d84caa94ed984cc309949ed7d25508
Lars Buntemeyer <lars.buntemeyer@hzg.de>
2021-11-18 22:33:00 +0100

    added center coordinate function

--- cordex/core/utils.py
+++ cordex/core/utils.py
@@ -7,6 +7,12 @@ import tempfile
 def get_tempfile():
    """Creates a temporay filename."""
    return tempfile.mkstemp()[1]
+
+
+def to_center_coordinate(ds):
+    ds.coords['lon'] = (ds.coords['lon'] + 180) % 360 - 180
+    return ds
+
 #
 #
 # def copy_dataset(src, varname=None, timestep=None, destination=None):
