2d675c53d5fdce712a743b20917f64e921b9d985
github action <lars.buntemeyer@hzg.de>
2022-03-31 21:02:26 +0200

    added pole utils

--- cordex/core/utils.py
+++ cordex/core/utils.py
@@ -1,7 +1,9 @@
 #! /usr/bin/python
 # coding: utf-8
 
+
 import tempfile
+from . import cf
 
 
 def get_tempfile():
@@ -12,6 +14,19 @@ def get_tempfile():
 def to_center_coordinate(ds):
     ds.coords["lon"] = (ds.coords["lon"] + 180) % 360 - 180
     return ds
+
+
+def pole(ds):
+    """Returns rotated pole longitude and latitude"""
+    pole_lon = ds[cf.DEFAULT_MAPPING_NCVAR].grid_north_pole_longitude
+    pole_lat = ds[cf.DEFAULT_MAPPING_NCVAR].grid_north_pole_latitude
+    return pole_lon, pole_lat
+
+
+def pole_crs(ds):
+    """Return a cartropy RotatedPole instance"""
+    from cartopy.crs import RotatedPole
+    return RotatedPole(*pole(ds))
 
 
 #
