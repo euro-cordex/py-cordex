1e702e751f216705040d6f1d9fc5e24c9fd497dd
Lars Buntemeyer <lars.buntemeyer@hereon.de>
2022-04-06 13:26:54 +0200

    updates tests

--- tests/test_tables.py
+++ tests/test_tables.py
@@ -3,7 +3,13 @@ import pytest
 
 
 @pytest.mark.parametrize(
-    "table", ["Amon", "day", "1hr", "3hr", "CV", "coordinate", "fx", "grids"]
+    "table", ["CMIP6_Amon", "CMIP6_day", "CMIP6_E1hr", "CMIP6_E3hr", "CMIP6_coordinate", "CMIP6_fx", "CMIP6_grids"]
 )
-def test_download_tables(table):
-    cx.cordex_cmor_table(table)
+def test_download_cmip6_cmor_tables(table):
+    cx.tables.cmip6_cmor_table(table)
+
+@pytest.mark.parametrize(
+    "table", ["CORDEX_CV", "CORDEX_remo_example"]
+)
+def test_download_cordex_cmor_tables(table):
+    cx.tables.cordex_cmor_table(table)
