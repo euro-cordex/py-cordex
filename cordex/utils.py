b50db4611b4acf515961c1ac086dbcfcd309dab6
Lars Buntemeyer <larsbuntemeyer@gmail.com>
2023-02-18 10:14:08 +0100

    added _guess_info

--- cordex/utils.py
+++ cordex/utils.py
@@ -1,5 +1,9 @@
 import tempfile
 
+import numpy as np
+
+import cordex as cx
+
 
 def get_tempfile():
     """Creates a temporay filename."""
@@ -9,3 +13,61 @@ def get_tempfile():
 def to_center_coordinate(ds):
     ds.coords["lon"] = (ds.coords["lon"] + 180) % 360 - 180
     return ds
+
+
+def _get_info(ds, tables=None):
+    if tables is None:
+        tables = cx.domains.table
+    try:
+        x = ds.cf["X"]
+        y = ds.cf["Y"]
+    except KeyError:
+        x = ds.rlon
+        y = ds.rlat
+    nlon = x.size
+    nlat = y.size
+    dlon = x.diff(x.dims[0]).values.item(0)
+    dlat = y.diff(y.dims[0]).values.item(0)
+    ll_lon = x[0].min().values.item(0)
+    ll_lat = y[0].min().values.item(0)
+    ur_lon = x[-1].min().values.item(0)
+    ur_lat = y[-1].min().values.item(0)
+    try:
+        pollon = ds.cf["grid_mapping"].grid_north_pole_longitude
+        pollat = ds.cf["grid_mapping"].grid_north_pole_latitude
+    except KeyError:
+        pollon = None
+        pollat = None
+    coords = {
+        "nlon": nlon,
+        "nlat": nlat,
+        "ll_lon": ll_lon,
+        "ur_lon": ur_lon,
+        "ll_lat": ll_lat,
+        "ur_lat": ur_lat,
+        "dlon": dlon,
+        "dlat": dlat,
+        "pollon": pollon,
+        "pollat": pollat,
+    }
+    # round
+    info = {
+        k: (np.round(v, 14) if isinstance(v, float) else v) for k, v in coords.items()
+    }
+    return info
+
+
+def _guess_domain(ds, tables=None):
+    if tables is None:
+        tables = cx.domains.table
+    info = _get_info(ds, tables)
+    filt = tables
+    for k, v in info.items():
+        if filt.empty:
+            return None
+        if v:
+            filt = filt[np.isclose(filt[k], v)]
+        else:
+            filt = filt[np.isnan(filt[k])]  # | filt[k] is None]
+    # reset index and convert to dict
+    return filt.reset_index().iloc[0].to_dict()
