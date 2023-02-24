c8f8629ac991b6658c26dbb84aed4f637ac8f4ef
Lars Buntemeyer <larsbuntemeyer@gmail.com>
2023-02-24 11:51:26 +0100

    updated accessor docstrings

--- cordex/accessor.py
+++ cordex/accessor.py
@@ -10,25 +10,42 @@ class CordexAccessor:
 
     @property
     def domain_id(self):
-        """Return the domain_id if possible."""
+        """Returns the domain_id."""
         return self._obj.attrs["CORDEX_domain"]
 
     @property
     def grid_mapping(self):
+        """Returns the grid_mapping variable."""
         return self._obj.cf["grid_mapping"]
 
     def info(self):
-        """Return domain info."""
+        """Return domain info in CORDEX format.
+
+        The function returns a dictionary containing domain
+        information in the format of the CORDEX archive specifications.
+
+        Returns
+        -------
+        domain info : dict
+            A dictionary that contains domain information.
+
+        """
         return _get_info(self._obj)
 
     def guess(self):
-        """Guess which domain."""
+        """Guess which domain this could be.
+
+        Compares the coordinate axis information to known the
+        coordinates of known CORDEX domains to guess the
+        ``domain_id``.
+
+        Returns
+        -------
+        domain info : dict
+            A dictionary that contains domain information.
+
+        """
         return _guess_domain(self._obj)
-
-
-#    def plot(self):
-#        """Plot data on a map."""
-#        return "plotting!"
 
 
 @xr.register_dataset_accessor("cx")
