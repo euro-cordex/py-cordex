60550c9655a65d0a4c5c51fba316e6956efcab94
Lars Buntemeyer <larsbuntemeyer@gmail.com>
2025-09-18 10:37:50 +0200

    failsafe for VG2500 server

--- tests/test_regions.py
+++ tests/test_regions.py
@@ -1,8 +1,13 @@
+import pytest
+import requests
 import cordex as cx
 
 from . import requires_geopandas
 
 
+@pytest.mark.skipif(
+    not requests.get("https://daten.gdz.bkg.bund.de").ok, reason="Service unavailable"
+)
 # naive download test
 @requires_geopandas
 def test_germany():
